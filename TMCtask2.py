6f9427751f3ffdf11749a5f0f31427ffb47f8345
Muhammad Muhib Amir <muhib.pro.310@gmail.com>
2026-07-22 15:03:03 +0500

    Add files via upload

---
+++ TMCtask2.py
@@ -0,0 +1,14 @@
+# Miles to Kilometer
+miles = float(input("Enter Miles: "))
+print("Miles to Kilometer:", miles * 1.60934)
+# Kilometer to Miles
+kilometers = float(input("Enter Kilometer: "))
+print("Kilometer to Miles:", kilometers * 0.621371)
+# Celsius to Fahrenheit
+celsius = float(input("Enter Celsius: "))
+fahrenheit = (celsius * 9 / 5) + 32
+print("Celsius to Fahrenheit:", fahrenheit)
+# Fahrenheit to Celsius
+fahrenheit = float(input("Enter Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5 / 9
+print("Fahrenheit to Celsius:", celsius)
